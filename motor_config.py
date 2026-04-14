1e3a171c692683aead754b9877188c8c754ce357
Justin Barwind <80134428+JustinBarwind@users.noreply.github.com>
2026-04-14 13:09:25 -0500

    Update motor_config.py

--- motor_config.py
+++ motor_config.py
@@ -1,1 +1,21 @@
+# MOTOR CONFIGURATION
 
+motors = {
+    "Motor_A": {
+        "name": "AeroTech G80",
+        "propellant_mass_lb": 0.220,   # lbm
+        "expected_impulse_lbf_s": None
+    },
+
+    "Motor_B": {
+        "name": "Estes F15",
+        "propellant_mass_lb": 0.110,
+        "expected_impulse_lbf_s": None
+    },
+
+    "Motor_C": {
+        "name": "Custom Test Motor",
+        "propellant_mass_lb": 0.300,
+        "expected_impulse_lbf_s": None
+    }
+}
